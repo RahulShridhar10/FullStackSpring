3ae4f96bda222357d380443eb7a7616ed1a4ceec
Rahul Shridhar <136424587+RahulShridhar10@users.noreply.github.com>
2024-12-03 11:34:06 +0530

    Add files via upload

---
+++ Fullstackspring/target/classes/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, description, done, target_date, username)
+values(10001,'Buy groceries',false, CURRENT_DATE(),'rahul');
+
+insert into todo(id, description, done, target_date, username)
+values(10002,'Hit the gym',false, CURRENT_DATE(),'rahul');
+
+insert into todo(id, description, done, target_date, username)
+values(10003,'Go for a walk',false, CURRENT_DATE(),'rahul');
